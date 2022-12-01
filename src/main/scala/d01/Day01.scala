188cb6825304104e822cb235e1b4db401697068d
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2022-12-01 14:47:48 +0100

    Day 1 solution and tests

--- src/main/scala/d01/Day01.scala
+++ src/main/scala/d01/Day01.scala
@@ -1,39 +1,42 @@
 package org.lafeuille.adventofcode.y2022
 package d01
 
+import scala.annotation.tailrec
 import scala.io.Source
+
+case class Elf(caloriesList: List[Int]) {
+  lazy val total: Int = caloriesList.sum
+}
 
 object Day01 {
   def myInput: Source =
     Source.fromURL(getClass.getResource("input.txt"))
 
-  lazy val myNumbers: List[Int] =
-    myInput.getLines.map(_.toInt).toList
+  @tailrec
+  private def rec(elves: List[Elf], remainingLines: List[String]): List[Elf] =
+    remainingLines match
+      case Nil => elves
+      case a :: tail if a.isBlank => rec(elves, tail)
+      case x => rec(Elf(x.takeWhile(!_.isBlank).map(_.toInt)) :: elves, x.dropWhile(!_.isBlank))
+
+  lazy val myElves: List[Elf] =
+    rec (Nil, myInput.getLines.toList)
 }
 
 object Part1 {
-  def pairs[T](list: List[T]): List[(T, T)] =
-    if list.isEmpty then Nil else
-      list zip list.tail
 
-  def result(numbers: List[Int]): Int =
-    pairs(numbers).count { case (a, b) => a < b }
+  def result(elves: List[Elf]): Int =
+    elves.map(_.total).max
 
   @main def runPart1(): Unit =
-    println(result(Day01.myNumbers))
+    println(result(Day01.myElves))
 }
 
 object Part2 {
-  def triplets[T](list: List[T]): List[(T, T, T)] =
-    if list.size < 3 then Nil else
-      list zip list.tail zip list.tail.tail map { case ((a, b), c) => (a, b, c) }
 
-  def tripletsSum(numbers: List[Int]): List[Int] =
-    triplets(numbers).map { case (a, b, c) => a + b + c }
-
-  def result(numbers: List[Int]): Int =
-    Part1.result(tripletsSum(numbers))
+  def result(elves: List[Elf]): Int =
+    elves.map(_.total).sorted.takeRight(3).sum
 
   @main def runPart2(): Unit =
-    println(result(Day01.myNumbers))
+    println(result(Day01.myElves))
 }
--- src/test/scala/d01/Day01Spec.scala
+++ src/test/scala/d01/Day01Spec.scala
@@ -4,42 +4,41 @@ package d01
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.*
 
-val sample = List(199, 200, 208, 210, 200, 207, 240, 269, 260, 263)
+val sample = List(
+  Elf(List(1000, 2000, 3000)),
+  Elf(List(4000)),
+  Elf(List(5000, 6000)),
+  Elf(List(7000, 8000, 9000)),
+  Elf(List(10000))
+)
 
 class Part1Spec extends AnyFlatSpec with should.Matchers {
 
-  "Sample list" should "have 7 measurements larger than the previous" in {
-    Part1.result(sample) shouldBe 7
+  val sampleResult = 24000
+
+  "Sample list" should s"have max $sampleResult" in {
+    Part1.result(sample) shouldBe sampleResult
   }
 
-  val expected = 1692
+  val inputResult = 67016
 
-  "My input" should s"have $expected measurements larger than the previous" in {
-    Part1.result(Day01.myNumbers) shouldBe 1692
+  "My input" should s"have $inputResult measurements larger than the previous" in {
+    Part1.result(Day01.myElves) shouldBe inputResult
   }
 
 }
 
 class Part2Spec extends AnyFlatSpec with should.Matchers {
-  val triplets = List((199, 200, 208), (200, 208, 210), (208, 210, 200), (210, 200, 207), (200, 207, 240), (207, 240, 269), (240, 269, 260), (269, 260, 263))
 
-  "Sample list" should s"have triplets $triplets" in {
-    Part2.triplets(sample) shouldBe triplets
+  val sampleResult = 45000
+
+  "Sample list" should s"have max $sampleResult" in {
+    Part2.result(sample) shouldBe sampleResult
   }
 
-  val tripletsSum = List(607, 618, 618, 617, 647, 716, 769, 792)
-
-  "Sample list" should s"have tripletSum of $tripletsSum" in {
-    Part2.tripletsSum(sample) shouldBe tripletsSum
-  }
-
-  it should "have 5 sums that are larger than the previous ones" in {
-    Part2.result(sample) shouldBe 5
-  }
-
-  val expected = 1724
+  val expected = 200116
 
   "My input" should s"have a part 2 result of $expected" in {
-    Part2.result(Day01.myNumbers) shouldBe expected
+    Part2.result(Day01.myElves) shouldBe expected
   }
 }
